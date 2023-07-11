3c518b83a315f21299f6e16a9924947d01708002
Nivedita <129652218+nivedita1811@users.noreply.github.com>
2023-07-11 22:36:15 +0530

    Update bb_uart.h

--- include/bb_uart.h
+++ include/bb_uart.h
@@ -1,193 +1,216 @@
+#include "nwy_bb_ble.h"
+#include "nwy_bb_osi_api.h"
+#include "nwy_bb_usb_serial.h"
+#include "nwy_bb_pm.h"
+
 /**
- * @file bytebeam_uart.h
- * @brief ByteBeam UART API functions.
+ * ==============================================================================================
+ * Enum to represent the type of UART parity. 
+ * 
+ * UART_PARITY_NONE: No parity is used.
+ * UART_PARITY_EVEN: Even parity is used.
+ * UART_PARITY_ODD: Odd parity is used.
+ * ==============================================================================================
  */
-
-#ifndef BYTEBEAM_UART_H
-#define BYTEBEAM_UART_H
-
-#include <stdint.h>
-#include <stdbool.h>
-
-/** UART parity options. */
-typedef enum uart_parity {
-    UART_PARITY_NONE,   /**< No parity */
-    UART_PARITY_EVEN,   /**< Even parity */
-    UART_PARITY_ODD     /**< Odd parity */
+typedef enum 
+{
+    UART_PARITY_NONE,   // No parity
+    UART_PARITY_EVEN,   // Even parity
+    UART_PARITY_ODD     // Odd parity
 } uart_parity_t;
 
-/** UART data bits options. */
-typedef enum uart_data_bits {
-    UART_DATA_BITS_5,   /**< 5 data bits */
-    UART_DATA_BITS_6,   /**< 6 data bits */
-    UART_DATA_BITS_7,   /**< 7 data bits */
-    UART_DATA_BITS_8    /**< 8 data bits */
+/**
+ * ==============================================================================================
+ * Enum to represent the size of UART data bits. 
+ * 
+ * UART_DATA_BITS_7: 7 data bits are used.
+ * UART_DATA_BITS_8: 8 data bits are used.
+ * ==============================================================================================
+ */
+typedef enum 
+{
+    UART_DATA_BITS_7,   // 7 data bits
+    UART_DATA_BITS_8    // 8 data bits
 } uart_data_bits_t;
 
-/** UART stop bits options. */
-typedef enum uart_stop_bits {
-    UART_STOP_BITS_1,   /**< 1 stop bit */
-    UART_STOP_BITS_1_5, /**< 1.5 stop bits */
-    UART_STOP_BITS_2    /**< 2 stop bits */
+/**
+ * ==============================================================================================
+ * Enum to represent the size of UART stop bits. 
+ * 
+ * UART_STOP_BITS_1: 1 stop bit is used.
+ * UART_STOP_BITS_2: 2 stop bits are used.
+ * ==============================================================================================
+ */
+typedef enum 
+{
+    UART_STOP_BITS_1,   // 1 stop bit
+    UART_STOP_BITS_2    // 2 stop bits
 } uart_stop_bits_t;
 
-/** UART mode options. */
-typedef enum nwy_uart_mode {
-    UART_MODE_BLOCKING,       /**< UART interface will block during data transfer */
-    UART_MODE_NON_BLOCKING,   /**< UART interface will not block during data transfer */
-    UART_MODE_INTERRUPT,      /**< UART interface will use interrupts for data transfer */
-    UART_MODE_POLLING,        /**< UART interface will use polling for data transfer */
-    UART_MODE_FULL_DUPLEX,    /**< UART interface will support simultaneous sending and receiving */
-    UART_MODE_HALF_DUPLEX     /**< UART interface will support either sending or receiving, but not at the same time */
+/**
+ * ==============================================================================================
+ * Enum to represent the mode of UART. 
+ * 
+ * UART_MODE_AT: AT mode is used.
+ * UART_MODE_DATA: Data mode is used.
+ * ==============================================================================================
+ */
+typedef enum 
+{
+    UART_MODE_AT = 0,
+    UART_MODE_DATA = 1
 } nwy_uart_mode_t;
 
-/** UART configuration structure. */
-typedef struct bytebeam_uart_config {
-    uint32_t name;                   /**< UART name. */
-    uint32_t baud;                   /**< Baud rate. */
-    uart_parity_t parity;            /**< Parity mode. */
-    uart_data_bits_t data_size;      /**< Data bits size. */
-    uart_stop_bits_t stop_size;      /**< Stop bits size. */
-    bool flow_ctrl;                  /**< Flow control. */
-    nwy_uart_mode_t mode;            /**< UART mode. */
-    void (*recv_cb)(uint8_t* data, uint32_t length); /**< Receive callback function. */
-    void (*tx_cb)(void);             /**< Transmit callback function. */
+/**
+ * ==============================================================================================
+ * Struct to represent the configuration of ByteBeam UART.
+ * 
+ * Contains configuration parameters including name, baud rate, parity, data size, stop size, 
+ * flow control, mode, and receive and transmit callbacks.
+ * ==============================================================================================
+ */
+typedef struct 
+{
+    uint32_t name;
+    uint32_t baud;
+    uart_parity_t parity;
+    uart_data_bits_t data_size;
+    uart_stop_bits_t stop_size;
+    bool flow_ctrl;
+    nwy_uart_mode_t mode; 
+    void (*recv_cb)(uint8_t* data, uint32_t length);
+    void (*tx_cb)(void);
 } bytebeam_uart_config_t;
 
 /**
- * @brief Initialize the UART interface.
- * @param config The UART configuration.
- * @return The UART handle, or -1 if initialization failed.
+=====================================================================================================================================================
+
+@enum Name           : uart_status
+@b Scope             : Public
+@n@n@b Description   : Enumerates the possible statuses after attempting UART initialization.
+@param Input Data    : None
+@return Return Value : None
+
+=====================================================================================================================================================
+*/
+typedef enum {
+    UART_INIT_OK = 0,               // UART initialized successfully
+    UART_INIT_NULL_CFG,             // Null configuration error during UART initialization
+    UART_INIT_FAIL,                 // UART initialization failed
+    UART_SET_BAUD_FAIL,             // Baud rate setting failed
+    UART_REG_CB_FAIL,               // Callback registration failed
+    UART_SET_TIMEOUT_FAIL,          // Timeout setting failed
+    UART_QUEUE_INIT_FAIL            // Queue initialization failed
+} uart_status;
+
+/**
+=====================================================================================================================================================
+
+@enum Name           : print_status
+@b Scope             : Public
+@n@n@b Description   : Enumerates the possible statuses after attempting to print.
+@param Input Data    : None
+@return Return Value : None
+
+=====================================================================================================================================================
+*/
+typedef enum {
+    PRINT_SUCCESS,                    // Print operation was successful
+    PRINT_FAILURE_NULL_POINTER,       // Failed due to null pointer
+    PRINT_FAILURE                     // Print operation failed
+} print_status;
+
+/**
+=====================================================================================================================================================
+
+@enum Name           : publish_status
+@b Scope             : Public
+@n@n@b Description   : Enumerates the possible statuses after attempting to publish.
+@param Input Data    : None
+@return Return Value : None
+
+=====================================================================================================================================================
+*/
+typedef enum {
+    PUBLISH_SUCCESS,                 // Publish operation was successful
+    PUBLISH_FAILURE_QUEUE_FULL,      // Failed due to the queue being full
+} publish_status;
+
+/**
+=====================================================================================================================================================
+
+@enum Name           : send_status
+@b Scope             : Public
+@n@n@b Description   : Enumerates the possible statuses after attempting to send.
+@param Input Data    : None
+@return Return Value : None
+
+=====================================================================================================================================================
+*/
+typedef enum {
+    SEND_SUCCESS,                    // Send operation was successful
+    SEND_FAILURE_SEMAPHORE,          // Failed due to semaphore
+} send_status;
+
+/**
+=====================================================================================================================================================
+
+@enum Name           : send_can_status
+@b Scope             : Public
+@n@n@b Description   : Enumerates the possible statuses after attempting to send over CAN.
+@param Input Data    : None
+@return Return Value : None
+
+=====================================================================================================================================================
+*/
+typedef enum {
+    SEND_CAN_SUCCESS,                // Send operation over CAN was successful
+    SEND_CAN_FAILURE,                // Send operation over CAN failed
+    SEND_CAN_SEMAPHORE_FAILURE       // Failed due to semaphore when attempting to send over CAN
+} send_can_status;
+
+
+/**
+ * ==============================================================================================
+ * Enum to represent the status of queue operation. 
+ * 
+ * QUEUE_PUT_SUCCESS: Queue operation was successful.
+ * QUEUE_PUT_FAILURE: Queue operation failed.
+ * QUEUE_PUT_FULL: Queue is full.
+ * QUEUE_PUT_NO_ENTRY: No entry in the queue.
+ * ==============================================================================================
+ */
+typedef enum {
+    QUEUE_PUT_SUCCESS,
+    QUEUE_PUT_FAILURE,
+    QUEUE_PUT_FULL,
+    QUEUE_PUT_NO_ENTRY
+} QueuePutStatus;
+
+/**
+ * ==============================================================================================
+ * These are the function prototypes for the Bytebeam UART interface. They include initialization,
+ * configuration (baud rate, parameters), data transmission and reception (in bytes, data blocks, 
+ * and single byte formats), registration of callbacks for data reception and transmission, 
+ * de-initialization, setting the receive frame timeout, and sending AT commands.
+ * 
+ * The specific details of each function, including parameters and return types, are provided in 
+ * their respective definitions in the source code.
+ * ==============================================================================================
  */
 int bytebeam_uart_init(bytebeam_uart_config_t config);
-
-/**
- * @brief Set the baud rate for the UART interface.
- * @param hd The UART handle.
- * @param baud The new baud rate.
- * @return True if the baud rate was successfully set, false otherwise.
- */
-bool bytebeam_uart_set_baud(uint8_t hd, uint32_t baud);
-
-/**
- * @brief Get the baud rate of the UART interface.
- * @param hd The UART handle.
- * @param baud Pointer to store the baud rate.
- * @return True if the baud rate was successfully retrieved, false otherwise.
- */
-bool bytebeam_uart_get_baud(uint8_t hd, uint32_t* baud);
-
-/**
- * @brief Set the parameters (parity, data bits, stop bits, flow control) of the UART interface.
- * @param hd The UART handle.
- * @param parity The parity mode.
- * @param data_size The data bits size.
- * @param stop_size The stop bits size.
- * @param flow_ctrl The flow control.
- * @return True if the parameters were successfully set, false otherwise.
- */
-bool bytebeam_uart_set_parameters(uint8_t hd, uart_parity_t parity, uart_data_bits_t data_size, uart_stop_bits_t stop_size, bool flow_ctrl);
-
-/**
- * @brief Get the parameters (parity, data bits, stop bits, flow control) of the UART interface.
- * @param hd The UART handle.
- * @param parity Pointer to store the parity mode.
- * @param data_size Pointer to store the data bits size.
- * @param stop_size Pointer to store the stop bits size.
- * @param flow_ctrl Pointer to store the flow control.
- * @return True if the parameters were successfully retrieved, false otherwise.
- */
-bool bytebeam_uart_get_parameters(uint8_t hd, uart_parity_t* parity, uart_data_bits_t* data_size, uart_stop_bits_t* stop_size, bool* flow_ctrl);
-
-/**
- * @brief Send data in bytes through the UART interface with a delay between each byte.
- * @param hd The UART handle.
- * @param data_ptr Pointer to the data to be sent.
- * @param length The length of the data to be sent.
- * @param delay_ms The delay between each byte in milliseconds.
- * @return The number of bytes sent, or -1 if the send operation failed.
- */
-int bytebeam_uart_send_data_in_bytes(uint8_t hd, uint8_t* data_ptr, uint32_t length, uint32_t delay_ms);
-
-/**
- * @brief Send datacontinued...
-
-/**
- * @brief Send data block through the UART interface.
- * @param hd The UART handle.
- * @param data_ptr Pointer to the data to be sent.
- * @param length The length of the data to be sent.
- * @return The number of bytes sent, or -1 if the send operation failed.
- */
-int bytebeam_uart_send_data_block(uint8_t hd, uint8_t* data_ptr, uint32_t length);
-
-/**
- * @brief Send a single byte through the UART interface.
- * @param hd The UART handle.
- * @param data The byte to be sent.
- * @return The number of bytes sent, or -1 if the send operation failed.
- */
-int bytebeam_uart_send_single_byte(uint8_t hd, uint8_t data);
-
-/**
- * @brief Receive data in bytes using a callback function.
- * @param recv_cb The receive callback function.
- * @return True if the receive operation was successful, false otherwise.
- */
+bool bytebeam_uart_set_baud(uint8_t uart_handle, uint32_t baud);
+bool bytebeam_uart_get_baud(uint8_t uart_handle, uint32_t* baud);
+bool bytebeam_uart_set_parameters(uint8_t uart_handle, uart_parity_t parity, uart_data_bits_t data_size, uart_stop_bits_t stop_size, bool flow_ctrl);
+bool bytebeam_uart_get_parameters(uint8_t uart_handle, uart_parity_t* parity, uart_data_bits_t* data_size, uart_stop_bits_t* stop_size, bool* flow_ctrl);
+int bytebeam_uart_send_data_in_bytes(uint8_t uart_handle, uint8_t* data_ptr, uint32_t length, uint32_t delay_ms);
+int bytebeam_uart_send_data_block(uint8_t uart_handle, uint8_t* data_ptr, uint32_t length);
+int bytebeam_uart_send_single_byte(uint8_t uart_handle, uint8_t data);
 bool bytebeam_receive_in_bytes(bytebeam_uart_recv_callback_t recv_cb);
-
-/**
- * @brief Receive data block using a callback function.
- * @param recv_cb The receive callback function.
- * @return True if the receive operation was successful, false otherwise.
- */
 bool bytebeam_receive_data_block(bytebeam_uart_recv_callback_t recv_cb);
-
-/**
- * @brief Receive a single byte using a callback function.
- * @param recv_cb The receive callback function.
- * @return True if the receive operation was successful, false otherwise.
- */
 bool bytebeam_receive_single_byte(bytebeam_uart_recv_callback_t recv_cb);
-
-/**
- * @brief Register a receive callback function for the UART interface.
- * @param hd The UART handle.
- * @param recv_cb The receive callback function.
- * @return True if the receive callback function was successfully registered, false otherwise.
- */
-bool bytebeam_uart_register_receive_callback(uint8_t hd, bytebeam_uart_recv_callback_t recv_cb);
-
-/**
- * @brief Register a transmit callback function for the UART interface.
- * @param hd The UART handle.
- * @param tx_cb The transmit callback function.
- * @return True if the transmit callback function was successfully registered, false otherwise.
- */
-bool bytebeam_uart_register_transmit_callback(uint8_t hd, bytebeam_uart_send_callback_t tx_cb);
-
-/**
- * @brief Deinitialize the UART interface.
- * @param hd The UART handle.
- * @return True if the UART interface was successfully deinitialized, false otherwise.
- */
-bool bytebeam_uart_deinit(uint8_t hd);
-
-/**
- * @brief Set the receive frame timeout for the UART interface.
- * @param hd The UART handle.
- * @param time The receive frame timeout value.
- * @return True if the receive frame timeout was successfully set, false otherwise.
- */
-bool bytebeam_set_rx_frame_timeout(uint8_t hd, int time);
-
-/**
- * @brief Send data through the AT UART port.
- * @param port The AT UART port.
- * @param data Pointer to the data to be sent.
- * @param size The size of the data to be sent.
- * @return True if the send operation was successful, false otherwise.
- */
+bool bytebeam_uart_register_receive_callback(uint8_t uart_handle, bytebeam_uart_recv_callback_t recv_cb);
+bool bytebeam_uart_register_transmit_callback(uint8_t uart_handle, bytebeam_uart_send_callback_t tx_cb);
+bool bytebeam_uart_deinit(uint8_t uart_handle);
+bool bytebeam_set_rx_frame_timeout(uint8_t uart_handle, int time);
 bool bytebeam_at_uart_send(bytebeam_uart_port_t port, void* data, size_t size);
-
-#endif /* BYTEBEAM_UART_H */
